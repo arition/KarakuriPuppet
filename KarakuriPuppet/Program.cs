4167da25ea4fc00c88443d329fc9a5251583a855
arition <aritionkb@gmail.com>
2018-06-13 22:22:59 -0700

    Parsing options from command line

--- KarakuriPuppet/Program.cs
+++ KarakuriPuppet/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using CommandLine;
 using KarakuriPuppetLib;
 
 namespace KarakuriPuppet
@@ -12,10 +13,16 @@ namespace KarakuriPuppet
     {
         static void Main(string[] args)
         {
-            var puppet = new Puppet();
-            puppet.Start("0.0.0.0", 8888, "1212");
-            Console.ReadKey();
-            puppet.Stop();
+            Parser.Default.ParseArguments<Option>(args).WithParsed(opt =>
+            {
+                var puppet = new Puppet();
+                puppet.Start("0.0.0.0", opt.Port, opt.Token);
+                Console.WriteLine($"Server runs on: 0.0.0.0:{opt.Port}. Press Ctrl+C to stop.");
+                while (true)
+                {
+                    Console.ReadKey(true);
+                }
+            });
         }
     }
 }
